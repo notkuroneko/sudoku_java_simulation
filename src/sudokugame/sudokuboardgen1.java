dec74e748bf562e0ac2a8040613b868882731766
Laplace Darknesss <133151430+notkuroneko@users.noreply.github.com>
2023-12-26 14:54:35 +0700

    Delete src/sudokugame directory

--- src/sudokugame/sudokuboardgen1.java
+++
@@ -1,8 +0,0 @@
-public class sudokuboardgen1 {
-    static {
-        System.load("libprojectbtlcpp.so");
-    }
-    native void boardgen();
-
-    //export CLASSPATH=/Documents/vscode/btlktlt    
-}
