e9848f0147e6fc534810270d24fcfa28b2af05c9
Laplace Darknesss <133151430+notkuroneko@users.noreply.github.com>
2024-01-05 18:33:35 +0700

    Add files via upload

--- btl/src/sudoku.java
+++ btl/src/sudoku.java
@@ -5,12 +5,12 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.io.File;
-import java.io.FileNotFoundException; 
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class sudoku{
     private JFrame f; 
-    private JButton b1,b2,b3,b4,b5,b6,b7,b8;
+    private JButton b1,b2,b3,b4,b5,b6,b7,b8,b9;
     private JScrollPane s1,s2;
     private Font presetNum = new Font(Font.DIALOG, Font.BOLD, 24);
     private Font filledNum = new Font(Font.DIALOG, Font.PLAIN, 24);
@@ -34,12 +34,23 @@ public class sudoku{
         public void setChosen(boolean x) {
             this.isChosen=x;
         }
+        public int getValue() {
+            return this.value;
+        }
         public void setValue(int x){
             this.value=x;
         }
         public void setAnswer(int x){
             this.answer=x;
         }
+        public boolean isCorrect(){
+            if (this.value==this.answer) return true;
+            else return false;
+        }
+        public void setCorrectAnswer(){
+            this.value=this.answer;
+            this.setText(Integer.toString(this.answer));
+        } //for testing purposes only.
     };
     private class Box extends JComponent{
         public void paint(Graphics g){
@@ -56,7 +67,39 @@ public class sudoku{
             g.drawLine(100,430,640,430);
         }
     };
-    private SudokuBox board[][];
+    private class gameState {
+        private SudokuBox board[][];
+        public gameState(){
+            this.board = new SudokuBox[10][10]; //init board squares
+            for (int i=1; i<=9; i++) {
+            for (int j=1; j<=9; j++)
+            {
+                this.board[i][j] = new SudokuBox();
+                this.board[i][j].setBackground(Color.white);
+                this.board[i][j].setBounds(102+60*(i-1),72+60*(j-1),56,56);
+                this.board[i][j].setBorderPainted(false);
+                this.board[i][j].setVisible(false);
+                this.board[i][j].setFont(filledNum);
+                f.add(this.board[i][j]);
+            } }
+            this.board[0][0] = new SudokuBox();
+            this.board[0][0].setChosen(false);
+        }
+        public boolean checkFinished() {
+            for (int i=1; i<=9; i++) {
+            for (int j=1; j<=9; j++) {
+                if (!this.board[i][j].isCorrect()) return false;
+            }}
+            return true;
+        }
+        public void cheatCode() {
+            for (int i=1; i<=9; i++) {
+            for (int j=1; j<=9; j++) {
+                this.board[i][j].setCorrectAnswer();
+            }
+            }
+        }
+    };
     private class InputListener extends KeyAdapter {}
     private int previ=0,prevj=0;
 
@@ -90,6 +133,10 @@ public class sudoku{
         b8.setBounds(700,300,160,80);
         b8.setVisible(false);
         b8.setFont(big);
+        b9 = new JButton("You Did It!"); //init You Did It! answer button
+        b9.setBounds(700,200,160,80);
+        b9.setVisible(false);
+        b9.setFont(medium);
         b6 = new JButton("Delete History"); //init delete game history button
         b6.setBounds(400,450,160,80);
         b6.setVisible(false);
@@ -107,20 +154,7 @@ public class sudoku{
         s2 = new JScrollPane(); //init game history pane
         s2.setBounds(300,150,360,250);
         s2.setVisible(false);
-        board = new SudokuBox[10][10]; //init board squares
-        for (int i=1; i<=9; i++) {
-        for (int j=1; j<=9; j++)
-        {
-            board[i][j] = new SudokuBox();
-            board[i][j].setBackground(Color.white);
-            board[i][j].setBounds(102+60*(i-1),72+60*(j-1),56,56);
-            board[i][j].setBorderPainted(false);
-            board[i][j].setVisible(false);
-            board[i][j].setFont(filledNum);
-            f.add(board[i][j]);
-        } }
-        board[0][0] = new SudokuBox();
-        board[0][0].setChosen(false);
+        gameState g = new gameState();
         b1.addActionListener(new ActionListener() { //start game button, generates board and stack
             public void actionPerformed(ActionEvent e){
                 b1.setVisible(false);
@@ -132,12 +166,12 @@ public class sudoku{
                 b8.setVisible(true);
                 for (int i=1; i<=9; i++) {
                 for (int j=1; j<=9; j++) {
-                    board[i][j].setVisible(true);
-                    board[i][j].setFocusable(true);
-                    board[i][j].setValue(0);
-                    board[i][j].setText(" ");
-                    board[i][j].setChosen(false);
-                    board[i][j].setBackground(Color.white);
+                    g.board[i][j].setVisible(true);
+                    g.board[i][j].setFocusable(true);
+                    g.board[i][j].setValue(0);
+                    g.board[i][j].setText(" ");
+                    g.board[i][j].setChosen(false);
+                    g.board[i][j].setBackground(Color.white);
                 } }     
                 new sudokuboardgen1().boardgen(); //boardgen
                 try {
@@ -147,11 +181,11 @@ public class sudoku{
                     for (int j=1; j<=9; j++) {
                         String dataStr = myReader.nextLine();
                         int dataInt = Integer.parseInt(dataStr);
-                        if (dataInt!=0) board[j][i].setPresetSquare(dataInt); 
+                        if (dataInt!=0) g.board[j][i].setPresetSquare(dataInt);
                     } }  
                     for (int i=1; i<=9; i++) { 
                     for (int j=1; j<=9; j++) {
-                        board[j][i].setAnswer(Integer.parseInt(myReader.nextLine()));
+                        g.board[j][i].setAnswer(Integer.parseInt(myReader.nextLine()));
                     } }
                     myReader.close();
                   } 
@@ -160,44 +194,62 @@ public class sudoku{
                     ee.printStackTrace();
                   } //scan file for boardgen output (riddle and answer)
                 new sudokuboardgen1().fileDelete();
+                //g.cheatCode();
                 for (int i=1; i<=9; i++) { 
                 for (int j=1; j<=9; j++) {
-                    if (board[i][j].canChoose) {
+                    if (g.board[i][j].canChoose) {
                         final int tempi = i;
                         final int tempj = j;
-                        board[i][j].addActionListener(new ActionListener() {    
+                        g.board[i][j].addActionListener(new ActionListener() {    
                             public void actionPerformed(ActionEvent e){
-                                board[tempi][tempj].setChosen(!board[tempi][tempj].getChosen());
-                                if (board[tempi][tempj].getChosen()) {
-                                    board[tempi][tempj].setBackground(Color.LIGHT_GRAY);
-                                    if (board[previ][prevj].getChosen()) {
-                                        board[previ][prevj].setChosen(false);
-                                        board[previ][prevj].setBackground(Color.white);
+                                g.board[tempi][tempj].setChosen(!g.board[tempi][tempj].getChosen());
+                                if (g.board[tempi][tempj].getChosen()) {
+                                    g.board[tempi][tempj].setBackground(Color.LIGHT_GRAY);
+                                    if (g.board[previ][prevj].getChosen()) {
+                                        g.board[previ][prevj].setChosen(false);
+                                        g.board[previ][prevj].setBackground(Color.white);
                                     }
-                                    board[tempi][tempj].addKeyListener(new InputListener(){
+                                    g.board[tempi][tempj].addKeyListener(new InputListener(){
                                         public void keyPressed(KeyEvent e) {
-                                            if (e.getKeyChar()=='1') {board[tempi][tempj].setValue(1); board[tempi][tempj].setText("1");}
-                                            if (e.getKeyChar()=='2') {board[tempi][tempj].setValue(2); board[tempi][tempj].setText("2");}
-                                            if (e.getKeyChar()=='3') {board[tempi][tempj].setValue(3); board[tempi][tempj].setText("3");}
-                                            if (e.getKeyChar()=='4') {board[tempi][tempj].setValue(4); board[tempi][tempj].setText("4");}
-                                            if (e.getKeyChar()=='5') {board[tempi][tempj].setValue(5); board[tempi][tempj].setText("5");}
-                                            if (e.getKeyChar()=='6') {board[tempi][tempj].setValue(6); board[tempi][tempj].setText("6");}
-                                            if (e.getKeyChar()=='7') {board[tempi][tempj].setValue(7); board[tempi][tempj].setText("7");}
-                                            if (e.getKeyChar()=='8') {board[tempi][tempj].setValue(8); board[tempi][tempj].setText("8");}
-                                            if (e.getKeyChar()=='9') {board[tempi][tempj].setValue(9); board[tempi][tempj].setText("9");}
-                                            if (e.getKeyCode()==KeyEvent.VK_BACK_SPACE) {board[tempi][tempj].setValue(0); board[tempi][tempj].setText("");}
+                                            if (e.getKeyChar()=='1') {g.board[tempi][tempj].setValue(1); g.board[tempi][tempj].setText("1");}
+                                            if (e.getKeyChar()=='2') {g.board[tempi][tempj].setValue(2); g.board[tempi][tempj].setText("2");}
+                                            if (e.getKeyChar()=='3') {g.board[tempi][tempj].setValue(3); g.board[tempi][tempj].setText("3");}
+                                            if (e.getKeyChar()=='4') {g.board[tempi][tempj].setValue(4); g.board[tempi][tempj].setText("4");}
+                                            if (e.getKeyChar()=='5') {g.board[tempi][tempj].setValue(5); g.board[tempi][tempj].setText("5");}
+                                            if (e.getKeyChar()=='6') {g.board[tempi][tempj].setValue(6); g.board[tempi][tempj].setText("6");}
+                                            if (e.getKeyChar()=='7') {g.board[tempi][tempj].setValue(7); g.board[tempi][tempj].setText("7");}
+                                            if (e.getKeyChar()=='8') {g.board[tempi][tempj].setValue(8); g.board[tempi][tempj].setText("8");}
+                                            if (e.getKeyChar()=='9') {g.board[tempi][tempj].setValue(9); g.board[tempi][tempj].setText("9");}
+                                            if (e.getKeyCode()==KeyEvent.VK_BACK_SPACE) {g.board[tempi][tempj].setValue(0); g.board[tempi][tempj].setText("");}
+                                            if (g.checkFinished()) {
+                                                b9.setVisible(true);
+                                                g.board[tempi][tempj].setChosen(false);
+                                                for (int ii=1; ii<9; ii++)
+                                                for (int jj=1; jj<9; jj++) g.board[ii][jj].setFocusable(false);
+                                            }
+                                            /*try {
+                                                FileWriter myWriter = new FileWriter("boardlog.txt");
+                                                for (int i=1; i<=9; i++) { 
+                                                for (int j=1; j<=9; j++) {
+                                                myWriter.write(String.valueOf(g.board[i][j].getValue())+" ");
+                                                }
+                                                myWriter.write("\n");
+                                                }
+                                                myWriter.close();
+                                            } catch (IOException f) {
+                                                    System.out.println("An error occurred.");
+                                                    f.printStackTrace();
+                                            }*/
                                         }
                                     });
                                 }
-                                else {board[tempi][tempj].setBackground(Color.white);}
+                                else {g.board[tempi][tempj].setBackground(Color.white);}
                                 previ=tempi;
                                 prevj=tempj;
                             }
                     });
-                }
-                }
-                }
-            }
+                }}
+            }}
         });
         b2.addActionListener(new ActionListener() { //load game button
             public void actionPerformed(ActionEvent e) {
@@ -234,9 +286,10 @@ public class sudoku{
                 b.setVisible(false);
                 b5.setVisible(false);
                 b8.setVisible(false);
+                b9.setVisible(false);
                 for (int i=1; i<=9; i++) {
                 for (int j=1; j<=9; j++) {
-                    board[i][j].setVisible(false);
+                    g.board[i][j].setVisible(false);
                 } }      
             }
         });
@@ -255,6 +308,7 @@ public class sudoku{
                 s2.setVisible(false);
                 b6.setVisible(false);
                 b7.setVisible(false);
+                b9.setVisible(false);
             }
         });
         b8.addActionListener(new ActionListener() { //save game button
@@ -271,14 +325,15 @@ public class sudoku{
         f.add(b6);
         f.add(b7);
         f.add(b8);
+        f.add(b9);
         f.add(s1);
         f.add(s2);
         f.setVisible(true);
     }
 
     public static void main(String args[]) throws Exception{
+        {System.out.println(System.getProperty("java.library.path"));}
         new sudoku();
-        //System.out.println(System.getProperty("java.library.path"));
     }  
 }
 
